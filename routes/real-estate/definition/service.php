f91995269b6d5904977da1971c472660f9af2c42
Mussab Ali <mussabali19@gmail.com>
2022-06-24 13:04:09 +0500

    Update service.php

--- routes/real-estate/definition/service.php
+++ routes/real-estate/definition/service.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use \App\Http\Controllers\RealEstate\Definition\ServiceController;
 
 //Temporary Route, will be removed when nav is set
 Route::resource('service', ServiceController::class);
