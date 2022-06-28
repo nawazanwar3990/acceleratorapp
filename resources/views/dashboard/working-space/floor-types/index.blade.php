5eaf932f3bd843cdbf7859195ad9b07aa3d2d3e2
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-06-28 15:18:30 +0500

    Table Heading for Package

--- resources/views/dashboard/working-space/floor-types/index.blade.php
+++ resources/views/dashboard/working-space/floor-types/index.blade.php
@@ -6,7 +6,7 @@
                 @include('dashboard.components.general.form-list-header',['url'=>'dashboard.floor-types.create','is_create'=>true])
                 <div class="card-body">
                     <table class="table table-bordered table-hover">
-                        @include('dashboard.components.general.table-headings',['headings'=>\App\Enum\TableHeadings\FlatManagement\FloorType::getTranslationKeys()])
+                        @include('dashboard.components.general.table-headings',['headings'=>\App\Enum\TableHeadings\FlatManagement\DurationLeftNavEnum::getTranslationKeys()])
                         <tbody>
                         @include('dashboard.working-space.floor-types.list')
                         </tbody>
