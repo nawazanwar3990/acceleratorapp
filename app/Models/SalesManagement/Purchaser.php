b090891ce5d2941a87195f65ecfc1e45276d22f4
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-07-03 16:15:58 +0500

    Refactor

---
+++ app/Models/SalesManagement/Purchaser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models\SalesManagement;
+use App\Models\UserManagement\Hr;
+use App\Models\UserManagement\User;
+use App\Models\WorkingSpace\Building;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Purchaser extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'sales_id',
+        'hr_id',
+        'percentage',
+        'created_by',
+        'updated_by',
+        'building_id',
+    ];
+
+    public function building(): BelongsTo
+    {
+        return $this->belongsTo(Building::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function deletedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+
+    public function Hr(): BelongsTo
+    {
+        return $this->belongsTo(Hr::class);
+    }
+
+    public function sale(): BelongsTo
+    {
+        return $this->belongsTo(Sale::class);
+    }
+}
