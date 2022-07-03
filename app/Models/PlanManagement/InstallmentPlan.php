2f88f3405ee26ca926336022aca42ffe6c5afdae
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-07-03 16:09:33 +0500

    Refactor

---
+++ app/Models/PlanManagement/InstallmentPlan.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models\PlanManagement;
+
+use App\Models\UserManagement\User;
+use App\Models\WorkingSpace\Building;
+use App\Models\WorkingSpace\Flat;
+use App\Models\WorkingSpace\Floor;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InstallmentPlan extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'building_id',
+        'floor_id',
+        'flat_id',
+        'name',
+        'months',
+        'installment_duration',
+        'total_installments',
+        'reminder_days',
+        'down_payment_type',
+        'down_payment_value',
+        'penalties',
+        'first_penalty',
+        'first_penalty_days',
+        'first_penalty_type',
+        'first_penalty_charges',
+        'second_penalty',
+        'second_penalty_days',
+        'second_penalty_type',
+        'second_penalty_charges',
+        'third_penalty',
+        'third_penalty_days',
+        'third_penalty_type',
+        'third_penalty_charges',
+        'created_by',
+        'updated_by'
+    ];
+
+    public function building(): BelongsTo
+    {
+        return $this->belongsTo(Building::class);
+    }
+    public function floor(): BelongsTo
+    {
+        return $this->belongsTo(Floor::class);
+    }
+    public function flat(): BelongsTo
+    {
+        return $this->belongsTo(Flat::class);
+    }
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function deletedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+}
