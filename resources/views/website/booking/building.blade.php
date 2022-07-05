db169b7bf039e6bb4b75c65c8ea7aebc20a094f4
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-07-05 19:09:44 +0500

    booking forms

--- resources/views/website/booking/building.blade.php
+++ resources/views/website/booking/building.blade.php
@@ -16,13 +16,38 @@
                     <div class="col-lg-9 col-md-8 py-4 border-start">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="mb-0">{{ $pageTitle }}</h4>
+                                <h4 class="mb-0">{{ $pageTitle }} for {{ $model->name }}</h4>
                             </div>
                             <div class="card-body">
                                 {!! Form::model($model, ['url' =>route('website.booking.store'), 'method' => 'POST','files' => true, 'class' => 'form-horizontal']) !!}
                                 @method('PUT')
                                 @include('website.booking.fields.building')
                                 {!! Form::close() !!}
+                            </div>
+                            <div class="card-footer text-end">
+                               <div class="row">
+                                   <div class="col-lg-6">
+                                   </div>
+                                   <div class="col-lg-6">
+                                       <ul class="list-group-flush list-group  justify-content-end">
+                                           <li class="list-group-item d-flex no-block align-items-center">
+                                               <a class="d-flex no-block align-items-center">
+                                                   <i class="mdi mdi-gmail fs-4 me-2 d-flex align-items-center"></i> Sub Total
+                                               </a>
+                                               <span
+                                                   class="badge bg-success ms-auto">{{ $model->price }} {{ \App\Services\GeneralService::get_default_currency() }}</span>
+                                           </li>
+                                           <li class="list-group-item d-flex no-block align-items-center">
+                                               <a class="d-flex no-block align-items-center">
+                                                   <i class="mdi mdi-gmail fs-4 me-2 d-flex align-items-center"></i> Grand
+                                                   Total
+                                               </a>
+                                               <span
+                                                   class="badge bg-success ms-auto">{{ $model->price }} {{ \App\Services\GeneralService::get_default_currency() }}</span>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
                             </div>
                         </div>
                     </div>
