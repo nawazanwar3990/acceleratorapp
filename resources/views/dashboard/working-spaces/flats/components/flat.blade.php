701a8c74741937740b7e7ac7012cbe06df055c4c
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-07-05 16:58:07 +0500

    refactor Code

--- resources/views/dashboard/working-spaces/flats/components/flat.blade.php
+++ resources/views/dashboard/working-spaces/flats/components/flat.blade.php
@@ -34,8 +34,8 @@
         @enderror
     </div>
     <div class="col-3 mb-3">
-        {!!  Html::decode(Form::label('flat_type_id' ,__('general.flat_type').'<i class="text-danger">*</i>',['class'=>'col-form-label']))   !!}
-        {!!  Form::select('flat_type_id', \App\Services\FlatService::getFlatTypesForDropdown(),null,['id'=>'flat_type_id',
+        {!!  Html::decode(Form::label('type_id' ,__('general.flat_type').'<i class="text-danger">*</i>',['class'=>'col-form-label']))   !!}
+        {!!  Form::select('type_id', \App\Services\FlatService::getFlatTypesForDropdown(),null,['id'=>'type_id',
             'class'=>'select2 form-control form-select', 'placeholder'=>__('general.flat_type'),'style'=>'width:100%', 'required'])
         !!}
         @error('flat_type_id')
@@ -99,6 +99,34 @@
         @enderror
     </div>
     <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('length' ,__('general.length').'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
+        {!!  Form::number('length',null,['min' => 1, 'step' => 'any', 'id'=>'length','class'=>'form-control ','placeholder'=>__('general.length'), 'required',  'onchange' => 'calculateArea();']) !!}
+        @error('length')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('width' ,__('general.width').'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
+        {!!  Form::number('width',null,['min' => 1, 'step' => 'any', 'id'=>'width','class'=>'form-control ','placeholder'=>__('general.width'), 'required', 'onchange' => 'calculateArea();']) !!}
+        @error('width')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('height' ,__('general.height').'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
+        {!!  Form::number('height',null,['min' => 1, 'step' => 'any', 'id'=>'height','class'=>'form-control ','placeholder'=>__('general.height'), 'required']) !!}
+        @error('height')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('area' ,__('general.area'),['class'=>'form-label']))   !!}
+        {!!  Form::text('area',null,['id'=>'area','class'=>'form-control ','placeholder'=>'0','readonly', 'required']) !!}
+        @error('area')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-3 mb-3">
         {!!  Html::decode(Form::label('longitude' ,__('general.longitude'),['class'=>'form-label']))   !!}
         {!!  Form::text('longitude',null,['id'=>'longitude',
             'class'=>'select2 form-control', 'placeholder'=>__('general.longitude'),'style'=>'width:100%'])
@@ -117,6 +145,30 @@
          style="{{ isset($for) ? ($model->furnished == true ? '' : 'display:none;'): 'display: none;' }}">
         {!!  Html::decode(Form::label('furnished_details' ,__('general.details') ,['class'=>'col-form-label']))   !!}
         {!!  Form::text('furnished_details',null,['id'=>'furnished_details','class'=>'form-control ','placeholder'=>__('general.details')]) !!}
+    </div>
+    <div class="col-3 mb-3 pt-3">
+        <div class="custom-control custom-radio">
+            {!!  Form::radio('rate_type','sft', isset($model) ? ($model->rate_type == 'sft' ? true: false) : true,['id'=>'rate_type_sft','class'=>'form-check-input rate-type']) !!}
+            {!!  Html::decode(Form::label('rate_type_sft' ,__('general.rate_square_feet'),['class'=>'form-check-label']))   !!}
+        </div>
+        <div class="custom-control custom-radio">
+            {!!  Form::radio('rate_type','lumpsum', isset($model) ? ($model->rate_type == 'lumpsum' ? true: false) : false,['id'=>'rate_type_lumpsum','class'=>'form-check-input rate-type']) !!}
+            {!!  Html::decode(Form::label('rate_type_lumpsum' ,__('general.lumpsum'),['class'=>'form-check-label']))   !!}
+        </div>
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('rate_square_feet' ,__('general.sale_rate_square_feet'),['class'=>'form-label']))   !!}
+        {!!  Form::number('rate_square_feet',null,['id'=>'rate_square_feat','class'=>'form-control ','placeholder'=>'0.00', 'onkeyup' => 'calculateArea();', 'onchange' => 'calculateArea();', 'required']) !!}
+        @error('rate_square_feat')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('total_amount' ,__('general.sales_price'),['class'=>'form-label']))   !!}
+        {!!  Form::text('total_amount',null,['id'=>'total_amount','class'=>'form-control ','placeholder'=>'0.00', 'readonly', 'autocomplete'=>'off']) !!}
+        @error('total_amount')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
     </div>
 </div>
 
--- resources/views/dashboard/working-spaces/flats/fields.blade.php
+++ resources/views/dashboard/working-spaces/flats/fields.blade.php
@@ -1,4 +1,3 @@
 @include('dashboard.working-spaces.flats.components.flat')
-@include('dashboard.working-spaces.flats.components.area-amount')
 @include('dashboard.working-spaces.components.services')
 @include('dashboard.working-spaces.components.images')
