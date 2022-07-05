2cb5c091da1762075f7573c997aa66bfc8bb466d
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-07-05 18:27:24 +0500

    refact;r

--- resources/views/website/booking/fields/flats.blade.php
+++ resources/views/website/booking/fields/flats.blade.php
@@ -1,43 +1,81 @@
-<div class="row">
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('name' ,__('general.name') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('name',null,['id'=>'name','class'=>'form-control','placeholder'=>__('general.name'), 'readonly']) !!}
+<div class="row mb-2">
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('building_id' ,__('general.building_name') ,['class'=>'col-form-label']))   !!}
+        {!!  Form::text('building_id',$model->building->name??null,['id'=>'building_id',
+            'class'=>' form-control', 'placeholder'=>__('general.ph_building_name'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('length' ,__('general.length_ft') ,['class'=>'form-label']))   !!}
-        {!!  Form::number('length',null,['id'=>'length','class'=>'form-control ','placeholder'=>__('general.length'), 'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('floor_id' ,__('general.floor_name') ,['class'=>'col-form-label']))   !!}
+        {!!  Form::text('floor_id',$model->floor->name??null,['id'=>'floor_id',
+            'class'=>' form-control', 'placeholder'=>__('general.ph_floor_name'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('width' ,__('general.width_ft') ,['class'=>'form-label']))   !!}
-        {!!  Form::number('width',null,['id'=>'width','class'=>'form-control ','placeholder'=>__('general.width'), 'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('name' ,__('general.flat_name') ,['class'=>'col-form-label']))   !!}
+        {!!  Form::text('name',null,['id'=>'name','class'=>'form-control ','placeholder'=>__('general.flat_name'), 'readonly']) !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('area' ,__('general.saleable_area_sft') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('area',null,['id'=>'area','class'=>'form-control ','placeholder'=>__('general.area'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('number' ,__('general.flat_number') ,['class'=>'col-form-label']))   !!}
+        {!!  Form::number('number',null,['id'=>'number','class'=>'form-control ','placeholder'=>__('general.flat_number'),'readonly']) !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('building_corners' ,__('general.building_corners') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('building_corners',\App\Services\BuildingService::buildingCornersForDropdown($model->building_corners),['id'=>'area','class'=>'form-control ','placeholder'=>__('general.building_corners'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('type_id' ,__('general.flat_type'),['class'=>'col-form-label']))   !!}
+        {!!  Form::text('type_id',$model->type??null,['id'=>'type_id',
+            'class'=>' form-control', 'placeholder'=>__('general.flat_type'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('building_type' ,__('general.building_type') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('building_type',\App\Services\BuildingService::buildingTypesForDropdown($model->building_type),['id'=>'building_type','class'=>'form-control ','placeholder'=>__('general.building_type'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('price' ,__('general.price'),['class'=>'col-form-label']))   !!}
+        {!!  Form::text('price',null,['id'=>'price',
+            'class'=>' form-control', 'placeholder'=>__('general.price'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('property_type' ,__('general.property_type') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('property_type',\App\Services\BuildingService::buildingPropertyTypesForDropdown($model->property_type),['id'=>'property_type','class'=>'form-control ','placeholder'=>__('general.property_type'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('facing' ,__('general.facing'),['class'=>'col-form-label']))   !!}
+        {!!  Form::text('facing',\App\Services\FlatService::facingDropdown($model->facing),['id'=>'facing',
+            'class'=>' form-control', 'placeholder'=>__('general.ph_facing'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('entry_gates' ,__('general.entry_gates') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('entry_gates',\App\Services\BuildingService::buildingEntryGatesForDropdown($model->entry_gates),['id'=>'entry_gates','class'=>'form-control ','placeholder'=>__('general.entry_gates'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('view' ,__('general.view_location'),['class'=>'col-form-label']))   !!}
+        {!!  Form::text('view', \App\Services\FlatService::getFlatViewsForDropdown($model->view),['id'=>'view',
+            'class'=>' form-control', 'placeholder'=>__('general.ph_view_location'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('no_of_floors' ,__('general.no_of_floors') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('no_of_floors',\App\Services\BuildingService::buildingNoOfFloorsForDropdown($model->no_of_floors),['id'=>'no_of_floors','class'=>'form-control ','placeholder'=>__('general.no_of_floors'),'readonly']) !!}
+    <div class="col-4 mb-3">
+        {!!  Html::decode(Form::label('accommodation' ,__('general.number_of_accommodation'),['class'=>'col-form-label']))   !!}
+        {!!  Form::text('accommodation',null,['id'=>'accommodation',
+            'class'=>' form-control', 'placeholder'=>__('general.ph_number_of_accommodation'),'readonly'])
+        !!}
     </div>
-    <div class="col-md-3 mb-3">
-        {!!  Html::decode(Form::label('facing' ,__('general.facing') ,['class'=>'form-label']))   !!}
-        {!!  Form::text('facing',\App\Services\BuildingService::buildingFacingsForDropdown($model->facing),['id'=>'facing','class'=>'form-control ','placeholder'=>__('general.facing'),'readonly']) !!}
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('latitude' ,__('general.latitude'),['class'=>'form-label']))   !!}
+        {!!  Form::text('latitude',null,['id'=>'latitude',
+            'class'=>' form-control', 'placeholder'=>__('general.latitude'),'readonly'])
+        !!}
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('length' ,__('general.length'),['class'=>'form-label']))   !!}
+        {!!  Form::number('length',null,['min' => 1, 'step' => 'any', 'id'=>'length','class'=>'form-control ','placeholder'=>__('general.length'), 'required',  'onchange' => 'calculateArea();']) !!}
+    </div>
+    <div class="col-2 mb-3">
+        {!!  Html::decode(Form::label('width' ,__('general.width'),['class'=>'form-label']))   !!}
+        {!!  Form::number('width',null,['min' => 1, 'step' => 'any', 'id'=>'width','class'=>'form-control ','placeholder'=>__('general.width'), 'required', 'onchange' => 'calculateArea();']) !!}
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('height' ,__('general.height'),['class'=>'form-label']))   !!}
+        {!!  Form::number('height',null,['min' => 1, 'step' => 'any', 'id'=>'height','class'=>'form-control ','placeholder'=>__('general.height'), 'required']) !!}
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('area' ,__('general.area'),['class'=>'form-label']))   !!}
+        {!!  Form::text('area',null,['id'=>'area','class'=>'form-control ','placeholder'=>'0','readonly', 'required']) !!}
+    </div>
+    <div class="col-3 mb-3">
+        {!!  Html::decode(Form::label('longitude' ,__('general.longitude'),['class'=>'form-label']))   !!}
+        {!!  Form::text('longitude',null,['id'=>'longitude',
+            'class'=>' form-control', 'placeholder'=>__('general.longitude'),'readonly'])
+        !!}
     </div>
 </div>
 @include('website.booking.fields.services',[
