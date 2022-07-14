0ffcadb6d2f57d093093b3f7470b071cdb21429a
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-07-14 14:47:59 +0500

    Refactor Code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,18 +8,14 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        $this->call(OfficeTypeSeeder::class);
-        $this->call(FloorTypeSeeder::class);
         $this->call(SettingSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(RoleUserSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(DurationSeeder::class);
-       /* $this->call(PackageSeeder::class);*/
         $this->call(ServiceSeeder::class);
-        /*$this->call(SubscriptionSeeder::class);
-        $this->call(BuildingSeeder::class);
-        $this->call(FloorSeeder::class);*/
+        $this->call(OfficeTypeSeeder::class);
+        $this->call(FloorTypeSeeder::class);
     }
 }
