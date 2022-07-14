454c085d5b0ced2b6b366ec9a59a0223d8823ae6
Faizan Amin <faakher@users.noreply.github.com>
2022-07-14 17:56:05 +0500

    Update MainNavEnum.php

--- app/Enum/LeftNavBar/MainNavEnum.php
+++ app/Enum/LeftNavBar/MainNavEnum.php
@@ -42,16 +42,16 @@ class MainNavEnum extends AbstractEnum
     {
         $routes = [
             self::DASHBOARD => '<i class="bx bxs-dashboard"></i>',
-            self::PACKAGE_MANAGEMENT => '<i class="bx bx-info"></i>',
-            self::SERVICE_MANAGEMENT => '<i class="bx bx-info"></i>',
-            self::BUSINESS_ACCELERATOR => '<i class="bx bx-chart-bar"></i>',
-            self::CUSTOMER_MANAGEMENT => '<i class="bx bx-chart-bar"></i>',
+            self::PACKAGE_MANAGEMENT => '<i class="bx bx-package"></i>',
+            self::SERVICE_MANAGEMENT => '<i class="bx bxs-customize"></i>',
+            self::BUSINESS_ACCELERATOR => '<i class="bx bx-user-circle"></i>',
+            self::CUSTOMER_MANAGEMENT => '<i class="bx bxs-user"></i>',
             self::INVESTOR_MANAGEMENT => '<i class="bx bx-chart-bar"></i>',
-            self::FREELANCER_MANAGEMENT => '<i class="bx bx-chart-bar"></i>',
-            self::PLAN_MANAGEMENT => '<i class="bx bx-info"></i>',
+            self::FREELANCER_MANAGEMENT => '<i class="bx bxs-face"></i>',
+            self::PLAN_MANAGEMENT => '<i class="bx bx-book-bookmark"></i>',
             self::SYSTEM_CONFIGURATION => '<i class="bx bx-briefcase"></i>',
-            self::USER_MANAGEMENT => '<i class="bx bx-shopping-cart"></i>',
-            self::CO_WORKING_SPACE => '<i class="bx bx-chart-bar"></i>',
+            self::USER_MANAGEMENT => '<i class="bx bx-user"></i>',
+            self::CO_WORKING_SPACE => '<i class="bx bxs-business"></i>',
             self::SALE_MANAGEMENT => '<i class="bx bx-chart-bar"></i>',
         ];
         if (!is_null($key) && array_key_exists($key, $routes)) {
