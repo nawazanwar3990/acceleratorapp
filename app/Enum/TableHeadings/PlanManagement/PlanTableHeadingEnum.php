bfa4f1fd62f7b857cd95fed12966afe72c526a70
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-07-18 11:28:21 +0500

    refactor plan

--- app/Enum/TableHeadings/PlanManagement/PlanTableHeadingEnum.php
+++ app/Enum/TableHeadings/PlanManagement/PlanTableHeadingEnum.php
@@ -9,6 +9,7 @@ use function __;
 class PlanTableHeadingEnum extends AbstractEnum
 {
     public const NAME = 'name';
+    public const SITTING_CAPACITY = 'sitting_capacity';
     public const DURATION = 'duration';
     public const PRICE = 'price';
     public const BASIC_SERVICE = 'basic_service';
@@ -22,6 +23,7 @@ class PlanTableHeadingEnum extends AbstractEnum
     {
         return [
             self::NAME => __(sprintf('%s.%s', 'general', self::NAME)),
+            self::SITTING_CAPACITY => __(sprintf('%s.%s', 'general', self::SITTING_CAPACITY)),
             self::DURATION => __(sprintf('%s.%s', 'general', self::DURATION)),
             self::PRICE => __(sprintf('%s.%s', 'general', self::PRICE)),
             self::BASIC_SERVICE => __(sprintf('%s.%s', 'general', self::BASIC_SERVICE)),
--- resources/views/dashboard/plan-management/plans/fields.blade.php
+++ resources/views/dashboard/plan-management/plans/fields.blade.php
@@ -1,16 +1,19 @@
 <div class="row mb-3">
-    {!!  Form::hidden('no_of_persons',1,['id'=>'no_of_persons', 'class'=>'form-control vertical-spin','required','readonly'])  !!}
-    <div class="col-md-4 mb-3">
+    <div class="col-md-3 mb-3">
         {!!  Html::decode(Form::label('name' ,__('general.name').'<i class="text-danger">*</i>' ,['class'=>'form-label']))   !!}
         {!!  Form::text('name',null,['id'=>'name','class'=>'form-control ','placeholder'=>__('general.name'), 'required']) !!}
     </div>
-    <div class="col-md-4 mb-3">
-        {!!  Html::decode(Form::label('duration' ,__('general.duration')." (".__('general.months').") ".'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
-        {!!  Form::text('duration',1,['id'=>'duration','class'=>'form-control ','placeholder'=>'0','required']) !!}
+    <div class="col-md-3 mb-3">
+        {!!  Html::decode(Form::label('no_of_persons' ,__('general.sitting_capacity')." (".trans('general.no_of_persons').") ".'<i class="text-danger">*</i>' ,['class'=>'form-label']))   !!}
+        {!!  Form::number('no_of_persons',1,['id'=>'no_of_persons','class'=>'form-control ','placeholder'=>__('general.no_of_persons'), 'required']) !!}
     </div>
-    <div class="col-md-4 mb-3">
+    <div class="col-md-3 mb-3">
+        {!!  Html::decode(Form::label('duration' ,__('general.duration')." (".__('general.months').") ".'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
+        {!!  Form::number('duration',1,['id'=>'duration','class'=>'form-control ','placeholder'=>'0','required']) !!}
+    </div>
+    <div class="col-md-3 mb-3">
         {!!  Html::decode(Form::label('price' ,__('general.price')." (".\App\Services\GeneralService::get_default_currency().") ".'<i class="text-danger">*</i>',['class'=>'form-label']))   !!}
-        {!!  Form::text('price',null,['id'=>'price','class'=>'form-control ','placeholder'=>'0','required']) !!}
+        {!!  Form::number('price',null,['id'=>'price','class'=>'form-control ','placeholder'=>'0','required']) !!}
     </div>
 </div>
 <div class="row">
