ae9ddc25bab96b779563d3406898a575d7ac6893
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-07-20 13:37:24 +0500

    test

--- resources/views/dashboard/user-management/customers/list.blade.php
+++ resources/views/dashboard/user-management/customers/list.blade.php
@@ -5,32 +5,12 @@
         <td>{{ $record->getFullName() }}</td>
         <td>{{ $record->email }}</td>
         @if( auth()->user()->hasRole(\App\Enum\RoleEnum::BUSINESS_ACCELERATOR))
-          {{--  <td class="text-center">
+            <td class="text-center">
                 <a class="btn btn-sm btn-success"
                    href="{{ route('dashboard.subscriptions.create',['id'=>$record->id,'type'=>\App\Enum\SubscriptionTypeEnum::PLAN]) }}">
                     {{__('general.subscriptions')}}
                 </a>
-            </td>--}}
-            <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true">Action</button>
-                <ul class="dropdown-menu" style="">
-                    <li>
-                        <a class="dropdown-item text-black-50" href="{{route('dashboard.subscriptions.create',['fId'=>$record->id])}}">
-                            {{__('general.offices')}}  ({{ count($record->offices) }})
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item text-black-50" href="{{ route('dashboard.floors.edit',$record->id) }}">
-                            {{__('general.edit')}}
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item text-black-50" href="{{ route('dashboard.floors.show',$record->id) }}">
-                            {{__('general.show')}}
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            </td>
         @endif
     </tr>
 @empty
