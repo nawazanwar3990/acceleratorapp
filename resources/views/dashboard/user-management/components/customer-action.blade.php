2872ed4d78ed45b7c93c41e1d620521eb23d4027
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-07-20 13:48:28 +0500

    test

--- resources/views/dashboard/user-management/components/customer-action.blade.php
+++ resources/views/dashboard/user-management/components/customer-action.blade.php
@@ -3,14 +3,12 @@
             aria-expanded="false" aria-haspopup="true">Action
     </button>
     <ul class="dropdown-menu">
-        @if(!$customer->already_subscription($customer->id,\App\Enum\SubscriptionTypeEnum::PLAN))
-            <li>
-                <a class="dropdown-item text-black-50"
-                   href="{{ route('dashboard.subscriptions.create',['id'=>$customer->id,'type'=>\App\Enum\SubscriptionTypeEnum::PLAN]) }}">
-                    {{__('general.apply_subscription')}}
-                </a>
-            </li>
-        @endif
+        <li>
+            <a class="dropdown-item text-black-50"
+               href="{{ route('dashboard.subscriptions.create',['id'=>$customer->id,'type'=>\App\Enum\SubscriptionTypeEnum::PLAN]) }}">
+                {{__('general.apply_subscription')}}
+            </a>
+        </li>
         <li>
             <a class="dropdown-item text-black-50" href="{{ route('dashboard.customers.edit',$customer->id) }}">
                 {{__('general.edit')}}
