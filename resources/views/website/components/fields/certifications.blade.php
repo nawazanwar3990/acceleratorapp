98ab8abf877eab1b084156aac2320142a040e1b1
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-08-02 10:02:06 +0500

    refactor Mentor

--- resources/views/website/components/fields/certifications.blade.php
+++ resources/views/website/components/fields/certifications.blade.php
@@ -4,41 +4,85 @@
     </div>
     <div class="card-body">
         <table class="table table-bordered table-sm">
-            <tbody>
-            <tr>
-                <td>
-                    {!! Form::label('certifications[certificate_name][]',__('general.certificate_name'),['class'=>'form-label']) !!}
-                    {!!  Form::text('certifications[certificate_name][]',null,['id'=>'certifications[certificate_name][]','class'=>'form-control','autocomplete'=>'off']) !!}
-                </td>
-                <td>
-                    {!! Form::label('certifications[institute][]',__('general.institute'),['class'=>'form-label']) !!}
-                    {!!  Form::text('certifications[institute][]',null,['id'=>'certifications[institute][]','class'=>'form-control']) !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('certifications[year][]',__('general.year'),['class'=>'form-label']) !!}
-                    {!!  Form::text('certifications[year][]',null,['id'=>'certifications[year][]','class'=>'form-control']) !!}
-                </td>
-                <td>
-                    {!! Form::label('certifications[any_distinction][]',__('general.any_distinction'),['class'=>'form-label']) !!}
-                    {!!  Form::text('certifications[any_distinction][]',null,['id'=>'certifications[any_distinction][]','class'=>'form-control']) !!}
-                </td>
-            </tr>
-            <td colspan="2" class="text-center">
-                <a href="javascript:void(0);"
-                   onclick="clone_table_body(this);"
-                   class="btn btn-xs btn-info">
-                    <i class="bx bx-plus"></i>
-                </a>
-                <a href="javascript:void(0);" tabindex="18"
-                   onclick="remove_table_body(this);"
-                   class="btn btn-xs btn-danger">
-                    <i class="bx bx-minus"></i>
-                </a>
-            </td>
-            </tr>
-            </tbody>
+            @if(isset($model->certifications) AND count($model->certifications)>0)
+                @foreach($model->certifications  as $certification)
+                    <tbody>
+                    <tr>
+                        <td>
+                            {!! Form::label('certifications[certificate_name][]',__('general.certificate_name'),['class'=>'form-label']) !!}
+                            {!!  Form::text('certifications[certificate_name][]',$certification->certificate_name,['id'=>'certifications[certificate_name][]','class'=>'form-control','autocomplete'=>'off']) !!}
+                        </td>
+                        <td>
+                            {!! Form::label('certifications[institute][]',__('general.institute'),['class'=>'form-label']) !!}
+                            {!!  Form::text('certifications[institute][]',$certification->institute,['id'=>'certifications[institute][]','class'=>'form-control']) !!}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            {!! Form::label('certifications[year][]',__('general.year'),['class'=>'form-label']) !!}
+                            {!!  Form::text('certifications[year][]',$certification->year,['id'=>'certifications[year][]','class'=>'form-control']) !!}
+                        </td>
+                        <td>
+                            {!! Form::label('certifications[any_distinction][]',__('general.any_distinction'),['class'=>'form-label']) !!}
+                            {!!  Form::text('certifications[any_distinction][]',$certification->any_distinction,['id'=>'certifications[any_distinction][]','class'=>'form-control']) !!}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="text-center">
+                            @if($loop->last)
+                                <a href="javascript:void(0);"
+                                   onclick="clone_table_body(this);"
+                                   class="btn btn-xs btn-info">
+                                    <i class="bx bx-plus"></i>
+                                </a>
+                            @endif
+                            <a href="javascript:void(0);" tabindex="18"
+                               onclick="remove_table_body(this);"
+                               class="btn btn-xs btn-danger">
+                                <i class="bx bx-minus"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                @endforeach
+            @else
+                <tbody>
+                <tr>
+                    <td>
+                        {!! Form::label('certifications[certificate_name][]',__('general.certificate_name'),['class'=>'form-label']) !!}
+                        {!!  Form::text('certifications[certificate_name][]',null,['id'=>'certifications[certificate_name][]','class'=>'form-control','autocomplete'=>'off']) !!}
+                    </td>
+                    <td>
+                        {!! Form::label('certifications[institute][]',__('general.institute'),['class'=>'form-label']) !!}
+                        {!!  Form::text('certifications[institute][]',null,['id'=>'certifications[institute][]','class'=>'form-control']) !!}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        {!! Form::label('certifications[year][]',__('general.year'),['class'=>'form-label']) !!}
+                        {!!  Form::text('certifications[year][]',null,['id'=>'certifications[year][]','class'=>'form-control']) !!}
+                    </td>
+                    <td>
+                        {!! Form::label('certifications[any_distinction][]',__('general.any_distinction'),['class'=>'form-label']) !!}
+                        {!!  Form::text('certifications[any_distinction][]',null,['id'=>'certifications[any_distinction][]','class'=>'form-control']) !!}
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="text-center">
+                        <a href="javascript:void(0);"
+                           onclick="clone_table_body(this);"
+                           class="btn btn-xs btn-info">
+                            <i class="bx bx-plus"></i>
+                        </a>
+                        <a href="javascript:void(0);" tabindex="18"
+                           onclick="remove_table_body(this);"
+                           class="btn btn-xs btn-danger">
+                            <i class="bx bx-minus"></i>
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            @endif
         </table>
     </div>
 </div>
