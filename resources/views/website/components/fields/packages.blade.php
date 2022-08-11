f65f660df30b51dfff7f9eadab9b832f4756b24d
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-11 14:56:06 +0500

    test

--- resources/views/website/components/fields/packages.blade.php
+++ resources/views/website/components/fields/packages.blade.php
@@ -25,7 +25,7 @@
                         <div class="price-table-content">
                             @foreach($package->services as $service_name=>$service_limit)
                                 <div class="price-row" style="padding: 10px 10px;font-size: 13px;text-align: left;">
-                                  @if($service_limit>0) <i class="bx bx-check text-success"></i> @else <i class="bx bx-time text-danger"></i> @endif {{ $service_name }} <strong class="w-bold pull-right">{{ $service_limit }}</strong>
+                                  @if($service_limit>0) ✓ @else ✗ @endif {{ $service_name }} <strong class="w-bold pull-right">{{ $service_limit=='∞'?trans('general.unlimited'):$service_limit }}</strong>
                                 </div>
                             @endforeach
                         </div>
