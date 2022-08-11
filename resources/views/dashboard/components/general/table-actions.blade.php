b607920f0ab8a36980af41d43bb0e8569a3e590e
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-08-11 11:46:56 +0500

    refactor code

--- resources/views/dashboard/components/general/table-actions.blade.php
+++ resources/views/dashboard/components/general/table-actions.blade.php
@@ -1,6 +1,6 @@
 <a class="btn btn-xs btn-info" href="{{ $edit }}">
     {{__('general.edit')}} <i class="bx bx-edit"></i>
 </a>
-<a class="btn btn-xs bg-danger" href="{{ $delete }}">
+{{--<a class="btn btn-xs bg-danger" href="{{ $delete }}">
     {{__('general.delete')}} <i class="bx bx-trash"></i>
-</a>
+</a>--}}
