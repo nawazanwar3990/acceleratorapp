ec285f856af862c5217035ed39bbb273e5c648dd
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-11 17:59:06 +0500

    refactor

--- resources/views/dashboard/mentors/index.blade.php
+++ resources/views/dashboard/mentors/index.blade.php
@@ -6,7 +6,7 @@
                 @include('dashboard.components.general.form-list-header',['url'=>'','is_create'=>false])
                 <div class="card-body">
                     <table class="table custom-datatable table-bordered table-hover">
-                        @include('dashboard.components.general.table-headings',['headings'=>\App\Enum\TableHeadings\BATableHeadingEnum::getTranslationKeys()])
+                        @include('dashboard.components.general.table-headings',['headings'=>\App\Enum\TableHeadings\MentorTableHeadingEnum::getTranslationKeys()])
                         <tbody>
                         @include('dashboard.mentors.list')
                         </tbody>
