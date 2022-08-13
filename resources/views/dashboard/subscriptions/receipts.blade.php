6e9cd1ce018db61aad5820f1f2e8e1e083e79c9a
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-13 15:51:51 +0500

    refactor code

--- resources/views/dashboard/subscriptions/receipts.blade.php
+++ resources/views/dashboard/subscriptions/receipts.blade.php
@@ -26,7 +26,8 @@
                                     </a>
                                 </td>
                                 <td class="text-center">
-                                    <a class="btn btn-sm btn-info  mx-1" {{--onclick="approved_subscription('{{ $record->subscription->id }}');"--}}>
+                                    <a class="btn btn-sm btn-info  mx-1"
+                                       onclick="approved_subscription('{{ $record->id}}','{{ $record->subscribed->id }}');">
                                         {{ trans('general.approved') }} <i class="bx bx-plus-circle"></i>
                                     </a>
                                 </td>
@@ -42,7 +43,81 @@
 @endsection
 @section('innerScript')
     <script>
-        function approved_subscription() {
+        function approved_subscription(subscription_id, subscribed_id) {
+            Swal.fire({
+                title: 'Approved Subscription',
+                html: `{!!  Html::decode(Form::label('payment_type' ,__('general.payment_type').'<i class="text-danger">*</i>' ,['class'=>'form-label'])) !!}{{ Form::select('payment_type',\App\Enum\PaymentTypeEnum::getTranslationKeys(),\App\Enum\PaymentTypeEnum::OFFLINE,['class'=>'form-control','id'=>'payment_type','placeholder'=>'Select Payment Type']) }}`,
+                confirmButtonText: 'Next',
+                focusConfirm: false,
+                preConfirm: () => {
+                    const payment_type = Swal.getPopup().querySelector('#payment_type').value
+                    if (!payment_type) {
+                        Swal.showValidationMessage(`First Choose Payment Type`)
+                    }
+                    return {
+                        payment_type: payment_type,
+                        subscription_id: subscription_id,
+                        subscribed_id: subscribed_id
+                    }
+                }
+            }).then((result) => {
+
+                let payment_type = result.value.payment_type;
+                let subscription_id = result.value.subscription_id;
+                let subscribed_id = result.value.subscribed_id;
+
+                if (payment_type === '{{ \App\Enum\PaymentTypeEnum::OFFLINE }}') {
+                    Swal.fire({
+                        title: 'Manage Payment',
+                        html: `{!!  Html::decode(Form::label('transaction_id' ,__('general.transaction_id').'<i class="text-danger">*</i>' ,['class'=>'form-label'])) !!}{{ Form::text('transaction_id',null,['class'=>'form-control','id'=>'transaction_id']) }}`,
+                        confirmButtonText: 'Submit',
+                        focusConfirm: false,
+                        preConfirm: () => {
+                            const transaction_id = Swal.getPopup().querySelector('#transaction_id').value
+                            if (!transaction_id) {
+                                Swal.showValidationMessage(`First Enter Transaction ID`)
+                            }
+                            return {
+                                transaction_id: transaction_id,
+                                subscription_id: subscription_id,
+                                subscribed_id: subscribed_id
+                            }
+                        }
+                    }).then((result) => {
+                        Swal.fire({
+                            html: '{!! __('general.request_wait') !!}',
+                            allowOutsideClick: () => !Swal.isLoading()
+                        });
+                        Swal.showLoading();
+                        let data = {
+                            'payment_type': payment_type,
+                            'transaction_id': result.value.transaction_id,
+                            'subscription_id': result.value.subscription_id,
+                            'subscription_type': '{{ \App\Enum\SubscriptionTypeEnum::PACKAGE }}',
+                            'subscribed_id': result.value.subscribed_id
+                        }
+                        $.ajax({
+                            url: "{{ route('dashboard.subscriptions.store') }}",
+                            method: 'POST',
+                            data: data,
+                            success: function (response) {
+                                if (response.status === true) {
+                                    location.assign(response.route);
+                                }
+                            },
+                            error: function (response) {
+                            }
+                        });
+                        console.log(data);
+                    });
+                } else {
+                    Swal.fire(
+                        'Pending!',
+                        'This will be don later!',
+                        'pending'
+                    )
+                }
+            });
         }
     </script>
 @endsection
