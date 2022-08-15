95bdbcb6f83217359d01cef8e04a3a52e9ea2fca
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-15 13:33:09 +0500

    refactor code

--- resources/views/dashboard/meetings/list.blade.php
+++ resources/views/dashboard/meetings/list.blade.php
@@ -23,23 +23,39 @@
             {{ $record->meeting_held_date }}
         </td>
         <td>
-            <p class="fs-13 mb-0">{{ trans('general.start_time') }} : {{ $record->meeting_start_time }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.end_time') }} : {{ $record->meeting_end_time }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.start_time') }} :</strong> {{ $record->meeting_start_time }}
+            </p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.end_time') }} :</strong> {{ $record->meeting_end_time }}</p>
         </td>
         <td>
-            <p class="fs-13 mb-0">{{ $record->meeting_mode }}</p>
-            <p class="fs-13 mb-0">{{ $record->meeting_description }}</p>
+            <p class="fs-13 mb-0">
+                <strong>{{ trans('general.type') }} : </strong>{{ $record->meeting_mode }}
+            </p>
+            @if($record->meeting_mode=='physical')
+                <p class="fs-13 mb-0">
+                    <strong>{{ trans('general.description') }} : </strong> {{ $record->meeting_description }}
+                </p>
+            @else
+                <p class="fs-13 mb-0">{{ $record->meeting_description }}</p>
+            @endif
         </td>
         <td>
-            <p class="fs-13 mb-0">{{ trans('general.client_id') }} : {{ $record->meeting_organized_for }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.name') }} : {{ $record->meeting_organized_for_name }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.contact') }} : {{ $record->meeting_organized_for_contact }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.email') }} : {{ $record->meeting_organized_for_email }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.client_id') }}
+                    :</strong> {{ $record->meeting_organized_for }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.name') }}
+                    :</strong> {{ $record->meeting_organized_for_name }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.contact') }}
+                    :</strong> {{ $record->meeting_organized_for_contact }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.email') }}
+                    :</strong> {{ $record->meeting_organized_for_email }}</p>
         </td>
         <td>
-            <p class="fs-13 mb-0">{{ trans('general.room_number') }} : {{ $record->meeting_organized_location }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.room_type') }} : {{ $record->meeting_organized_location_type }}</p>
-            <p class="fs-13 mb-0">{{ trans('general.sitting_capacity') }} : {{ $record->meeting_organized_location_capacity }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.room_number') }}
+                    :</strong> {{ $record->meeting_organized_location }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.room_type') }}
+                    :</strong> {{ $record->meeting_organized_location_type }}</p>
+            <p class="fs-13 mb-0"><strong>{{ trans('general.sitting_capacity') }}
+                    :</strong> {{ $record->meeting_organized_location_capacity }}</p>
         </td>
         <td class="text-center">
             @include('dashboard.components.general.table-actions', [
