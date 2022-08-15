e9b935a16aa90011e569950ca98cb362d02c14df
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-15 13:17:22 +0500

    test

--- resources/views/dashboard/components/general/table-headings.blade.php
+++ resources/views/dashboard/components/general/table-headings.blade.php
@@ -3,6 +3,9 @@
     <th class="text-center" style="min-width: 50px;">#</th>
     @foreach($headings as $key=>$value)
         <th style="min-width: 100px;">{{ $value }}</th>
+        @if(in_array($key,[\App\Enum\TableHeadings\MeetingTableHeadingEnum::MEETING_ORGANIZED_FOR,\App\Enum\TableHeadings\MeetingTableHeadingEnum::MEETING_ORGANIZED_LOCATION]))
+            <th style="min-width: 300px;">{{ $value }}</th>
+        @endif
     @endforeach
     <th class="text-center" style="min-width: 150px;">
         {{__('general.action')}}
