be00650aa538003038e9ac51b1b059233e247054
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-08-15 13:08:10 +0500

    test

--- resources/views/dashboard/meetings/list.blade.php
+++ resources/views/dashboard/meetings/list.blade.php
@@ -2,15 +2,22 @@
     <tr>
         <td class="text-center">{{ $loop->iteration }}</td>
         <td>
-            {{ $record->meeting_name}} <br>
-            @include('components.view-images-model',['model_id'=>$record->id,'images'=>$record->images])
-        </td>
-        <td>
-            @if($record->meeting_arranged_for)
-                {{ \App\Enum\MeetingArrangedForEnum::getTranslationKeyBy($record->meeting_arranged_for) }}
+            @if($record->meeting_type)
+                {{ str_replace('_',' ',$record->meeting_type) }}
             @else
                 --
             @endif
+        </td>
+        <td>
+            @if($record->meeting_sub_type)
+                {{ str_replace('_',' ',$record->meeting_sub_type) }}
+            @else
+                --
+            @endif
+        </td>
+        <td>
+            {{ $record->meeting_name}} <br>
+            @include('components.view-images-model',['model_id'=>$record->id,'images'=>$record->images])
         </td>
         <td>
             @if($record->meeting_type)
@@ -23,8 +30,15 @@
             {{ $record->meeting_held_date }}
         </td>
         <td>
-            {{ $record->meeting_start_time }}
+            <p class="fs-13 mb-0">{{ trans('general.start_time') }} : {{ $record->meeting_start_time }}</p>
+            <p class="fs-13 mb-0">{{ trans('general.end_time') }} : {{ $record->meeting_end_time }}</p>
         </td>
+        <td>
+            <p class="fs-13 mb-0">{{ $record->meeting_mode }}</p>
+            <p class="fs-13 mb-0">{{ $record->meeting_description }}</p>
+        </td>
+        <td></td>
+        <td></td>
         <td class="text-center">
             @include('dashboard.components.general.table-actions', [
                 'edit' => route('dashboard.meeting-rooms.edit', $record->id),
