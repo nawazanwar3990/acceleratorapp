630fa4ed4cd01bbc0b0dcc4117d75905d6354de0
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-08-31 17:23:01 +0500

    test

--- resources/views/website/startups/components/service-provider-company.blade.php
+++ resources/views/website/startups/components/service-provider-company.blade.php
@@ -1,12 +1,19 @@
 @if(is_object($records))
     @if(count($records))
         @foreach($records as $record)
-            <div class="column is-3">
-                <a class="support-card">
-                    <i class="im im-icon-Mail-Love"></i>
-                    <h3>Email Marketing</h3>
-                    <p>We answer your questions and solve your technical problems</p>
-                </a>
+            <div class="column">
+                <div class="feature-card card-md hover-inset has-text-centered">
+                    <div class="card-icon">
+                        <i class="im im-icon-Two-Windows"></i>
+                    </div>
+                    <div class="card-title">
+                        <h4>Web development</h4>
+                    </div>
+                    <div class="card-feature-description">
+                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
+                  cu harum inermis iudicabit.</span>
+                    </div>
+                </div>
             </div>
         @endforeach
     @endif
--- resources/views/website/startups/index.blade.php
+++ resources/views/website/startups/index.blade.php
@@ -19,104 +19,12 @@
             services-cards
             is-minimal is-vcentered is-gapless is-multiline
           ">
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered">
-                        <div class="card-icon">
-                            <i class="im im-icon-Two-Windows"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>Web development</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered">
-                        <div class="card-icon">
-                            <i class="im im-icon-Smartphone-4"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>Mobile development</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered">
-                        <div class="card-icon">
-                            <i class="im im-icon-T-Shirt"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>Branding</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered mb-20">
-                        <div class="card-icon">
-                            <i class="im im-icon-Laptop-Phone"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>Responsive design</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered mb-20">
-                        <div class="card-icon">
-                            <i class="im im-icon-Cart-Quantity"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>E-Commerce</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card -->
-                <div class="column">
-                    <div class="feature-card card-md hover-inset has-text-centered mb-20">
-                        <div class="card-icon">
-                            <i class="im im-icon-Colosseum"></i>
-                        </div>
-                        <div class="card-title">
-                            <h4>Graphic design</h4>
-                        </div>
-                        <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {{--<div class="container">
                 @if(request()->has('type'))
                     <div class="columns is-multiline">
                         @include(sprintf('website.startups.components.%s',str_replace('_','-',request()->query('type'))))
                     </div>
                 @endif
-            </div>--}}
+            </div>
         </div>
     </x-slot>
 </x-page-layout>
