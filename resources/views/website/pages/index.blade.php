67654a5af53c153aabfabef23642c5a97403895a
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-08-31 16:39:58 +0500

    test

--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -1,6 +1,22 @@
 <x-page-layout
     :page="$page">
     <x-slot name="content">
+        @if(count($page->sections)>0)
+            @foreach($page->sections as $section)
+                @php
+                    $html = $section->html;
+                @endphp
+                @if(str_contains($html, '%faq_data%'))
+                    @php $html =  str_replace('%faq_data%',require_once(resource_path()."/views/components/faqs.blade.php"),$html) @endphp
+                @endif
+                @if(str_contains($html, '%contact_form_data%'))
+                    @php $html =  str_replace('%contact_form_data%',include(resource_path()."/views/components/contact-form.blade.php"),$html) @endphp
+                @endif
 
+                @if(str_contains($html, '%about_us_data%'))
+                    @php $html =  str_replace('%about_us_data%',require_once(resource_path()."/views/components/about-us.blade.php"),$html) @endphp
+                @endif
+            @endforeach
+        @endif
     </x-slot>
 </x-page-layout>
