d0a50eaa67174a1bd8b8f7dbcf69dbadbf35759e
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-01 10:57:41 +0500

    rest

--- resources/views/website/startups/services/components/ba-individual.blade.php
+++ resources/views/website/startups/services/components/ba-individual.blade.php
@@ -1,17 +1,16 @@
 @if(is_object($startup_services))
     @if(count($startup_services)>0)
         @foreach($startup_services as $service)
-            <div class="column">
-                <div class="feature-card card-md hover-inset has-text-centered border">
-                    <div class="card-icon">
-                        <i class="im im-icon-Two-Windows"></i>
+            <div class="column is-one-quarter flex-portrait-4">
+                <div class="flex-card is-feature padding-20">
+                    <div class="icon-container is-first is-icon-reveal">
+                        <img src="{{ asset('assets/img/graphics/icons/time-teal.svg') }}"
+                             data-base-url="{{ asset('assets/img/graphics/icons/time-teal.svg') }}"
+                             data-extension=".svg" alt="">
                     </div>
-                    <div class="card-title">
-                        <h4>{{ $service->name }}</h4>
-                    </div>
-                    <div class="card-feature-description">
-                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
-                  cu harum inermis iudicabit.</span>
+                    <div class="content-container has-text-centered pb-4">
+                        <h3>{{ $service->name }}</h3>
+                        <p></p>
                     </div>
                     <div class="py-1">
                         <a href="" class="button  primary-btn btn-sm rounded raised">View</a>
--- resources/views/website/startups/services/index.blade.php
+++ resources/views/website/startups/services/index.blade.php
@@ -1,12 +1,8 @@
 <x-page-layout
     :page="$page">
     <x-slot name="content">
-        <div class="container pb-5">
-            <div class=" columnsservices-cards is-minimal is-vcentered is-gapless is-multiline">
-                <div class="columns is-multiline">
-                    @include(sprintf('website.startups.services.components.%s-%s',$startup_for,$startup_type))
-                </div>
-            </div>
+        <div class="columns is-multiline is-flex-portrait">
+            @include(sprintf('website.startups.services.components.%s-%s',$startup_for,$startup_type))
         </div>
     </x-slot>
 </x-page-layout>
