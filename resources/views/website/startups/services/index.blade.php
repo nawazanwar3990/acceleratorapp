7b2e43ad68c9c55eb146c7d3c7fb7c1e5a1f013c
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-01 10:59:09 +0500

    rest

--- resources/views/website/startups/services/index.blade.php
+++ resources/views/website/startups/services/index.blade.php
@@ -1,8 +1,12 @@
 <x-page-layout
     :page="$page">
     <x-slot name="content">
-        <div class="columns is-multiline is-flex-portrait">
-            @include(sprintf('website.startups.services.components.%s-%s',$startup_for,$startup_type))
+        <div id="start" class="section">
+            <div class="container">
+                <div class="columns is-multiline is-flex-portrait">
+                    @include(sprintf('website.startups.services.components.%s-%s',$startup_for,$startup_type))
+                </div>
+            </div>
         </div>
     </x-slot>
 </x-page-layout>
