165311a81c3e7d27a0ccdd29affe688c6c683336
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-01 10:51:32 +0500

    rest

---
+++ resources/views/website/startups/services/components/ba-individual.blade.php
@@ -0,0 +1,23 @@
+@if(is_object($services))
+    @if(count($services)>0)
+        @foreach($services as $service)
+            <div class="column">
+                <div class="feature-card card-md hover-inset has-text-centered border">
+                    <div class="card-icon">
+                        <i class="im im-icon-Two-Windows"></i>
+                    </div>
+                    <div class="card-title">
+                        <h4>{{ $service->name }}</h4>
+                    </div>
+                    <div class="card-feature-description">
+                            <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet
+                  cu harum inermis iudicabit.</span>
+                    </div>
+                    <div class="py-1">
+                        <a href="" class="button  primary-btn btn-sm rounded raised">View</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endif
+@endif
--- resources/views/website/startups/services/index.blade.php
+++ resources/views/website/startups/services/index.blade.php
@@ -2,27 +2,7 @@
     :page="$page">
     <x-slot name="content">
         <div class="container pb-5">
-            <div class="pricing-wrapper">
-                <div class="pricing-tabs mb-0">
-                 {{--   @foreach(\App\Enum\AccessTypeEnum::getStartups() as $access_key=>$access_value)
-                        @php
-                            $new_startup_for = \App\Services\GeneralService::generateStartupFor($access_key);
-                            $new_start_type = \App\Services\GeneralService::generateStartType($access_key);
-                        @endphp
-                        <a class="tab-item @if($new_startup_for==$startup_for AND $new_start_type==$startup_type) is-active @endif"
-                           href="{{ route('website.startups.index',[$new_startup_for,$new_start_type]) }}">
-                            <img src="{{ asset('assets/img/graphics/icons/custom-pricing-icon-5-green.svg') }}"
-                                 alt="{{$access_value}}">
-                            <span>{{ $access_value }}</span>
-                        </a>
-                    @endforeach--}}
-                </div>
-            </div>
-            <div class="
-            columns
-            services-cards
-            is-minimal is-vcentered is-gapless is-multiline
-          ">
+            <div class=" columnsservices-cards is-minimal is-vcentered is-gapless is-multiline">
                 <div class="columns is-multiline">
                     @include(sprintf('website.startups.services.components.%s-%s',$startup_for,$startup_type))
                 </div>
