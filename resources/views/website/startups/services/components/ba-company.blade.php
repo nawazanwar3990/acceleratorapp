bc2665075562d95561c5c55c1f494cdbe70d4fd3
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-01 12:35:19 +0500

    rest

---
+++ resources/views/website/startups/services/components/ba-company.blade.php
@@ -0,0 +1,29 @@
+@if(is_object($startup_services))
+    @if(count($startup_services)>0)
+        @foreach($startup_services as $service)
+            <div class="column is-one-quarter flex-portrait-4">
+                <div class="flex-card is-feature padding-20">
+                    <div class="icon-container is-first is-icon-reveal">
+                        <img src="{{ asset('assets/img/graphics/icons/time-teal.svg') }}"
+                             data-base-url="{{ asset('assets/img/graphics/icons/time-teal.svg') }}"
+                             data-extension=".svg" alt="">
+                    </div>
+                    <div class="content-container has-text-centered pb-4">
+                        <h3>{{ $service->name }}</h3>
+                        <p></p>
+                    </div>
+                    <div class="py-1 text-center">
+                        @if($service->slug =='incubator')
+                            <a href="{{route('website.buildings.index',$startup_id)}}" class="button  primary-btn btn-sm rounded raised">{{trans('general.building')}}</a>
+                            <a href="{{route('website.floors.index',$startup_id)}}" class="button  primary-btn btn-sm rounded raised">{{trans('general.floor')}}</a>
+                            <a href="{{route('website.offices.index',$startup_id)}}" class="button  primary-btn btn-sm rounded raised">{{trans('general.office')}}</a>
+                        @else
+                            <a href="" class="button  primary-btn btn-sm rounded raised">View</a>
+
+                        @endif
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endif
+@endif
