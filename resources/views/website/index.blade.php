4f7bed607cb56e56bf004b5115fe81cb5dbde5a6
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-13 19:54:46 +0500

    npw its work

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -67,52 +67,51 @@
                 </div>
             </div>
         </section>
-        <section class="my-5 py-5 we-offer-holder" style="background-image: url('/uploads/offer_bg.webp')">
+        <section class="my-5 py-5 how-its-work-holder" style="background-image: url('/uploads/back_how_it_works.webp')">
             <div class="container">
                 <div class="row">
                     <div class="col-12 text-center pb-3">
-                        <h1>What We Offer</h1>
-                        <h6>Build, launch and scale your startup with TAQADAM</h6>
+                        <h1>How It Works</h1>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row justify-content-center">
                     <div class="col-lg-3 col-md-6">
                         <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                            <img class="card-img-top img-responsive" src="/uploads/apply_now.webp"
                                  alt="Card image cap">
                             <div class="card-body">
-                                <h4 class="card-title text-primary">Business Accelerator</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to one-to-one sessions with a diverse pool of expert mentors.</p>
+                                <h4 class="card-title text-primary">Applications</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                            <img class="card-img-top img-responsive" src="/uploads/accelerator.webp"
                                  alt="Card image cap">
                             <div class="card-body">
-                                <h4 class="card-title text-danger">Freelancer</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to one-to-one sessions with a diverse pool of expert mentors.</p>
+                                <h4 class="card-title text-danger">Bootcamp</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                            <img class="card-img-top img-responsive" src="/uploads/growth.webp"
                                  alt="Card image cap">
                             <div class="card-body">
-                                <h4 class="card-title text-success">Service Provider</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to one-to-one sessions with a diverse pool of expert mentors.</p>
+                                <h4 class="card-title text-success">Accelerator</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                            <img class="card-img-top img-responsive" src="/uploads/showcase.webp"
                                  alt="Card image cap">
                             <div class="card-body">
-                                <h4 class="card-title text-info">Mentor</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to one-to-one sessions with a diverse pool of expert mentors.</p>
+                                <h4 class="card-title text-info">Showcase</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
                             </div>
                         </div>
                     </div>
