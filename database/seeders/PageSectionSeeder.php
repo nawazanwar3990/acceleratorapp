f17ab3aef610031529b8b60f7e1bfcbf85d1388d
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-14 13:19:34 +0500

    refactor code

--- database/seeders/PageSectionSeeder.php
+++ database/seeders/PageSectionSeeder.php
@@ -159,14 +159,130 @@ class PageSectionSeeder extends Seeder
             ], [
                 'order' => '4',
                 'page_id' => '1',
-                'html' => '',
+                'html' => ' <section class="my-5 py-5 meet-startup-holder" style="background-image: url(/uploads/meet_back.webp)">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-6 text-center pb-3">
+                        <h1 class="mb-4">Meet The Startups</h1>
+                        <h6 class="pt-4">Since its launch in 2016, more than 130 startups have graduated from TAQADAM.
+                            During that
+                            time, TAQADAM funded more than $10 million in non-dilutive funding to startup founders.
+                            Learn more about our dynamic community</h6>
+                    </div>
+                </div>
+            </div>
+        </section>',
                 'active' => true,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ], [
                 'order' => '5',
                 'page_id' => '1',
-                'html' => '',
+                'html' => ' <section class="my-5 py-5 counter-back-holder" style="background-image: url(/uploads/counter_back.webp)">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title green-box text-white">130+</h4>
+                                <p class="card-text text-green">Startups <br>Graduates</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title cayan-box text-white">500+</h4>
+                                <p class="card-text text-cayan">Entrepreneurs <br>supported</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title yellow-box text-white">8500+</h4>
+                                <p class="card-text text-yellow">Showcase <br>Attendees</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title text-white orange-box">800+</h4>
+                                <p class="card-text text-orange">Jobs <br>Created</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>',
+                'active' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'order' => '5',
+                'page_id' => '1',
+                'html' => '  <section class="my-5 py-5 industries-holder">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-6 text-center pb-3">
+                            <h1 class="mb-4">Disruptive Industries</h1>
+                            <h6 class="pt-4">We are industry agnostic – supporting startups from all industries and at all stages.</h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/mobilityy.jpg);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-12.webp" alt="">
+                                <h4>Mobality</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/energy.jpg);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-15.webp" alt="">
+                                <h4>Energy</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/logistics.jpg);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-10.webp" alt="">
+                                <h4>Logistics</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/WATER-1.webp);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-9.webp" alt="">
+                                <h4>Water</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/DIGITAL-1.webp);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-14.webp" alt="">
+                                <h4>Digital</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/FINTECH.webp);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-16.webp" alt="">
+                                <h4>FIN-Teach</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/CYBER.webp);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-13.webp" alt="">
+                                <h4>cyber security</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 position-relative" style="background: url(/uploads/HEALTH.webp);">
+                            <div class="position-absolute">
+                                <img src="/uploads/Asset-11.webp" alt="">
+                                <h4>health</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>',
                 'active' => true,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -6,57 +6,5 @@
                 {!! $section->html !!}
             @endforeach
         @endif
-        <section class="my-5 py-5 meet-startup-holder" style="background-image: url('/uploads/meet_back.webp')">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-6 text-center pb-3">
-                        <h1 class="mb-4">Meet The Startups</h1>
-                        <h6 class="pt-4">Since its launch in 2016, more than 130 startups have graduated from TAQADAM.
-                            During that
-                            time, TAQADAM funded more than $10 million in non-dilutive funding to startup founders.
-                            Learn more about our dynamic community</h6>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="my-5 py-5 counter-back-holder" style="background-image: url('/uploads/counter_back.webp')">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-3 text-center pb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title green-box text-white">130+</h4>
-                                <p class="card-text text-green">Startups <br>Graduates</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 text-center pb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title cayan-box text-white">500+</h4>
-                                <p class="card-text text-cayan">Entrepreneurs <br>supported</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 text-center pb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title yellow-box text-white">8500+</h4>
-                                <p class="card-text text-yellow">Showcase <br>Attendees</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3 text-center pb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title text-white orange-box">800+</h4>
-                                <p class="card-text text-orange">Jobs <br>Created</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
     </x-slot>
 </x-home-layout>
