7a7d3a64c66d0118f47decb562815fd49ec30ec7
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-14 17:32:51 +0500

    refactor cpde

--- resources/views/website/startups/offices/index.blade.php
+++ resources/views/website/startups/offices/index.blade.php
@@ -14,66 +14,56 @@
             <div class="container">
                 <div class="row">
                     @forelse($offices as $office)
-                        <div class="column is-one-third-tablet is-one-quarter-desktop is-one-fifth-fullhd">
-                            <div class="grid-product">
-                                <div class="grid-product-info">
-                                    <span>{{ $office->name }}</span>
-                                </div>
-                                <div class="grid-product-rating">
-                                    <i class="fas fa-star highlighted"></i>
-                                    <i class="fas fa-star highlighted"></i>
-                                    <i class="fas fa-star highlighted"></i>
-                                    <i class="fas fa-star highlighted"></i>
-                                    <i class="fas fa-star highlighted"></i>
-                                </div>
-                                <div class="grid-product-image">
-                                    <img src="{{ asset('images/icon/company.png') }}"
-                                         data-demo-src="{{ asset('images/icon/company.png') }}"
-                                         alt="{{ $office->name }}">
-                                </div>
-                                @if($office->building)
-                                    <p class="border-bottom pb-1 mb-2">
-                                        <strong>{{ trans('general.building') }}</strong>
-                                        <a href="{{ route('website.buildings.index',[$startup_id,'s'=>$office->building->id]) }}"
-                                           class="pull-right">
-                                            {{ $office->building->name }}
-                                        </a>
-                                    </p>
-                                @endif
-                                @if($office->floor)
-                                    <p class="border-bottom pb-1 mb-2">
-                                        <strong>{{ trans('general.floor') }}</strong>
-                                        <a href="{{ route('website.floors.index',[$startup_id,$office->floor->building->id??null,'s'=>$office->floor->id]) }}"
-                                           class="pull-right">
-                                            {{ $office->floor->name }}
-                                        </a>
-                                    </p>
-                                @endif
-                                <p class="border-bottom pb-1">
-                                    <strong>{{ trans('general.office_type') }}</strong>
-                                    <span class="pull-right">
+                        <div class="col-3">
+                            <div class="card">
+                                <img class="card-img-top site-border" src="/uploads/mobilityy.jpg" alt="Card image"
+                                     style="width:100%">
+                                <div class="card-body">
+                                    <h4 class="card-title">{{$office->name}}</h4>
+                                    @if($office->building)
+                                        <p class="border-bottom pb-1 mb-2">
+                                            <spam>{{ trans('general.building') }}</spam>
+                                            <a href="{{ route('website.buildings.index',[$startup_id,'s'=>$office->building->id]) }}"
+                                               class="pull-right">
+                                                {{ $office->building->name }}
+                                            </a>
+                                        </p>
+                                    @endif
+                                    @if($office->floor)
+                                        <p class="border-bottom pb-1 mb-2">
+                                            <spam>{{ trans('general.floor') }}</spam>
+                                            <a href="{{ route('website.floors.index',[$startup_id,$office->floor->building->id??null,'s'=>$office->floor->id]) }}"
+                                               class="pull-right">
+                                                {{ $office->floor->name }}
+                                            </a>
+                                        </p>
+                                    @endif
+                                    <p class="border-bottom pb-1">
+                                        <spam>{{ trans('general.office_type') }}</spam>
+                                        <span class="pull-right">
                                          {{ $office->type->name ?? '' }}
                                     </span>
-                                </p>
-                                <p class="border-bottom pb-1">
-                                    <strong>{{ trans('general.sitting_capacity') }}</strong>
-                                    <span class="pull-right">{{ $office->sitting_capacity }}</span>
-                                </p>
-                                @if($office->getOwnerId())
-                                    <p class="border-bottom pb-1">
-                                        <strong>{{ trans('general.owner') }}</strong>
-                                        <span class="pull-right">{{ $office->getOwnerName() }}</span>
                                     </p>
-                                @endif
-                                <div class="buttons text-center justify-content-center">
-                                    @if(\App\Services\OfficeService::already_subscribed($office->id))
-                                        <a class="button is-fullwidth">{{ trans('general.already_subscribed') }}</a>
-                                    @else
-                                        <a class="button is-fullwidth"
-                                           href="{{ route('website.office.plans.index',[$office->id]) }}">
-                                            {{ trans('general.subscription_plans') }}
-                                        </a>
+                                    <p class="border-bottom pb-1">
+                                        <strong>{{ trans('general.sitting_capacity') }}</strong>
+                                        <span class="pull-right">{{ $office->sitting_capacity }}</span>
+                                    </p>
+                                    @if($office->getOwnerId())
+                                        <p class="border-bottom pb-1">
+                                            <strong>{{ trans('general.owner') }}</strong>
+                                            <span class="pull-right">{{ $office->getOwnerName() }}</span>
+                                        </p>
                                     @endif
+                                    <div class="buttons text-center justify-content-center">
+                                        @if(\App\Services\OfficeService::already_subscribed($office->id))
+                                            <a class="view_services">{{ trans('general.already_subscribed') }}</a>
+                                        @else
+                                            <a class="view_services"
+                                               href="{{ route('website.office.plans.index',[$office->id]) }}">
+                                                {{ trans('general.subscription_plans') }}
+                                            </a>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
