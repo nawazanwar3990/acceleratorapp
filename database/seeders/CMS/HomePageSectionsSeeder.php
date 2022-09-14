9b3e8e688ff8ffaf99ee1c92266d658b577bd1be
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-09-14 11:40:22 +0500

    new boxes

--- database/seeders/CMS/HomePageSectionsSeeder.php
+++ database/seeders/CMS/HomePageSectionsSeeder.php
@@ -18,21 +18,135 @@ class HomePageSectionsSeeder extends Seeder
             [
                 'order' => '1',
                 'page_id' => '1',
-                'html' => '',
+                'html' => '<section class="my-5 py-5 about-us-holder" style="background-image: url(/uploads/about_background.webp)">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 col-lg-9 col-xxl-9">
+                        <iframe width="800" height="400" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0"
+                                allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>',
                 'active' => true,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ], [
                 'order' => '2',
                 'page_id' => '1',
-                'html' => '',
+                'html' => ' <section class="my-5 py-5 we-offer-holder" style="background-image: url(/uploads/offer_bg.webp)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center pb-3">
+                        <h1>What We Offer</h1>
+                        <h6>Build, launch and scale your startup with TAQADAM</h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-primary">Business Accelerator</h4>
+                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
+                                    one-to-one sessions with a diverse pool of expert mentors.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-danger">Freelancer</h4>
+                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
+                                    one-to-one sessions with a diverse pool of expert mentors.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-success">Service Provider</h4>
+                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
+                                    one-to-one sessions with a diverse pool of expert mentors.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-info">Mentor</h4>
+                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
+                                    one-to-one sessions with a diverse pool of expert mentors.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>',
                 'active' => true,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ], [
                 'order' => '3',
                 'page_id' => '1',
-                'html' => '',
+                'html' => ' <section class="my-5 py-5 how-its-work-holder" style="background-image: url(/uploads/back_how_it_works.webp)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center pb-3">
+                        <h1>How It Works</h1>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/uploads/apply_now.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-primary">Applications</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/uploads/accelerator.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-danger">Bootcamp</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/uploads/growth.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-success">Accelerator</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <img class="card-img-top img-responsive" src="/uploads/showcase.webp"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title text-info">Showcase</h4>
+                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>',
                 'active' => true,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -1,136 +1,59 @@
 <x-home-layout
     :page="$page">
     <x-slot name="content">
-        {{-- @if(count($page->sections)>0)
-             @foreach($page->sections as $section)
-                 {!! $section->html !!}
-             @endforeach
-         @endif--}}
-        <section class="my-5 py-5 about-us-holder" style="background-image: url('/uploads/about_background.webp')">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-9 col-lg-9 col-xxl-9">
-                        <iframe width="800" height="400" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0"
-                                allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="my-5 py-5 we-offer-holder" style="background-image: url('/uploads/offer_bg.webp')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center pb-3">
-                        <h1>What We Offer</h1>
-                        <h6>Build, launch and scale your startup with TAQADAM</h6>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-primary">Business Accelerator</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
-                                    one-to-one sessions with a diverse pool of expert mentors.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-danger">Freelancer</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
-                                    one-to-one sessions with a diverse pool of expert mentors.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-success">Service Provider</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
-                                    one-to-one sessions with a diverse pool of expert mentors.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/icons/ba.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-info">Mentor</h4>
-                                <p class="card-text text-white text-center fw-bold">Each startup gets access to
-                                    one-to-one sessions with a diverse pool of expert mentors.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="my-5 py-5 how-its-work-holder" style="background-image: url('/uploads/back_how_it_works.webp')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center pb-3">
-                        <h1>How It Works</h1>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/uploads/apply_now.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-primary">Applications</h4>
-                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/uploads/accelerator.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-danger">Bootcamp</h4>
-                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/uploads/growth.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-success">Accelerator</h4>
-                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <img class="card-img-top img-responsive" src="/uploads/showcase.webp"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-info">Showcase</h4>
-                                <p class="card-text text-center fw-bold">Applications close date May 21, 2022</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        @if(count($page->sections)>0)
+            @foreach($page->sections as $section)
+                {!! $section->html !!}
+            @endforeach
+        @endif
         <section class="my-5 py-5 meet-startup-holder" style="background-image: url('/uploads/meet_back.webp')">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-6 text-center pb-3">
                         <h1 class="mb-4">Meet The Startups</h1>
-                        <h6 class="pt-4">Since its launch in 2016, more than 130 startups have graduated from TAQADAM. During that
+                        <h6 class="pt-4">Since its launch in 2016, more than 130 startups have graduated from TAQADAM.
+                            During that
                             time, TAQADAM funded more than $10 million in non-dilutive funding to startup founders.
                             Learn more about our dynamic community</h6>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="my-5 py-5 counter-back-holder" style="background-image: url('/uploads/counter_back.webp')">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title green-box text-white">130+</h4>
+                                <p class="card-text text-green">Startups <br>Graduates</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title cayan-box text-white">500+</h4>
+                                <p class="card-text text-cayan">Entrepreneurs <br>supported</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title yellow-box text-white">8500+</h4>
+                                <p class="card-text text-yellow">Showcase <br>Attendees</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title text-white orange-box">800+</h4>
+                                <p class="card-text text-orange">Jobs <br>Created</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
