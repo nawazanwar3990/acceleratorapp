d4894c41084495346083ba139fa19d8eaaba3736
Muhammad Hamza <82260165+MuhammadHamza0147@users.noreply.github.com>
2022-09-21 06:05:26 -0700

    home page logo & footer fixing

--- resources/views/components/website-footer.blade.php
+++ resources/views/components/website-footer.blade.php
@@ -1,30 +1,30 @@
 <footer class="py-5" style="background-color: rgb(232, 228, 228);">
     <div class="container">
         <div class="row">
-            <div class="col-sm-4 col-sm-4 col-xs-12 align-self-center footer-logo">
+            <div class="col-md-4 col-md-4 col-xs-12 align-self-center footer-logo">
                 <img src="{{ asset('uploads/header_logo.png') }}" alt="">
             </div>
-            <div class="col-sm-4 col-sm-4 col-xs-12 nav-holder align-self-center">
+            <div class="col-md-4 col-md-4 col-xs-12 nav-holder align-self-center">
                 <div class="row footer-image">
                     <div class="col-6 col">
                         <a class="text-uppercase" style="color: #01023B;font-weight: bolder">About</a>
                         <img src="{{ asset('uploads/line-D1.png') }}" alt="">
                     </div>
-                    <div class="col-6 border-start col right">
+                    <div class="col-6 border-start col right text-end">
                         <a class="text-uppercase" style="color: #01023B;font-weight: bolder">News</a>
-                        <img src="{{ asset('uploads/line-D1.png') }}" alt="">
+                        <img class="image-right" src="{{ asset('uploads/line-D1.png') }}" alt="">
                     </div>
                     <div class="col-6 col">
                         <a class="text-uppercase" style="color: #01023B;font-weight: bolder">StartUps</a>
                         <img src="{{ asset('uploads/line-D1.png') }}" alt="">
                     </div>
-                    <div class="col-6 border-start col right">
+                    <div class="col-6 border-start col right text-end">
                         <a class="text-uppercase" style="color: #01023B;font-weight: bolder">Contact</a>
-                        <img src="{{ asset('uploads/line-D1.png') }}" alt="">
+                        <img class="image-right" src="{{ asset('uploads/line-D1.png') }}" alt="">
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 col-sm-4 col-xs-12 align-self-center text-center">
+            <div class="col-lg-4 col-lg-4 mt-5 col-xs-12 align-self-center text-center">
                 <h4 class="footer-text" style="color: #01023B;font-weight: bolder">Stay Updated with the latest news</h4>
                 <div class="footer-buttons mt-3 mb-4">
                     <a class="btn btn-primary btn-xs mx-2 rounded-0" style="background-color: #01023B!important;">Subscribed</a>
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -305,7 +305,8 @@
                             <div class="container">
                                 <div class="header-content">
                                     <div class="line"></div>
-                                    <h1>OPTIMUM TECH</h1>
+                                    <img src="{{asset('uploads/ot_logo.png')}}" style="width: 100%;max-width: 200px">
+{{--                                    <h1>OPTIMUM TECH</h1>--}}
                                     <h4>Our competent team of inquisitive designers, marketers, and developers are highly capable of strong leadership and problem-solving skills, task execution, and innovative strategy-making.</h4>
                                 </div>
                             </div>
@@ -317,10 +318,11 @@
                             <div class="container">
                                 <div class="header-content">
                                     <div class="line animated bounceInLeft"></div>
-                                    <h1>IT EMPIRE</h1>
+                                    <img src="{{asset('uploads/it-empire-logo.png')}}" style="width: 100%;max-width: 200px">
+{{--                                    <h1>IT EMPIRE</h1>--}}
                                     <h4>
                                         Since its inception, it has grown and progressed to be at the center stage of information and technology, providing effective computing strategies and solutions to Private and Government Organizations. IT Empire has played a major role in some of the largest IT projects in the country.
-                                        </h4>
+                                    </h4>
                                 </div>
                             </div>
                         </div>
@@ -331,7 +333,14 @@
                             <div class="container">
                                 <div class="header-content">
                                     <div class="line animated bounceInLeft"></div>
-                                    <h1>ANT SOFT</h1>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <img src="{{asset('uploads/ant-soft-logo.png')}}" style="width: 100%;max-width: 200px">
+                                        </div>
+{{--                                        <div class="col-9 mt-3">--}}
+{{--                                            <h1>ANT SOFT</h1>--}}
+{{--                                        </div>--}}
+                                    </div>
                                     <h4>ANT SOFT have been involved in multiple complex E-Commerce B2C integration solutions, Connecting commerce platforms with different third party services. Have also built different integrations regarding payment, shipping, analytics etc.</h4>
                                 </div>
                             </div>
