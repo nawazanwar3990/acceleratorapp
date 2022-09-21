ee940864e5191ad85412110a06dda30942503361
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-09-21 12:24:09 +0500

    dynamic blogs

--- resources/views/website/news/index.blade.php
+++ resources/views/website/news/index.blade.php
@@ -4,66 +4,23 @@
         <section>
             <div class="">
                 <div style="display: flex;flex-wrap: wrap">
-                    <div class="col-md-3">
-                        <div class="our-story wow fadeInUp"
-                             style="background: url({{ asset('uploads/blog_new1.jpeg') }}) no-repeat; background-position: center; background-size: cover;"
-                             data-wow-duration="0.5s" data-wow-delay="0.2s">
-                            <div class="">
-                                <div class="our-story-content1 wow fadeInUp" data-wow-duration="0.5s"
-                                     data-wow-delay="0.5s">
-                                    <div class="our-story-content-inner">
-                                        <p>Who Will Be Our Next Hero? Applications Are Open Now for Business Accelerator
-                                            Accelerator</p>
+                    @forelse($newses as $news)
+                        <div class="col-md-3">
+                            <div class="our-story wow fadeInUp"
+                                 style="background: url('{{ asset($news->image) }}') no-repeat center;background-size: cover;"
+                                 data-wow-duration="0.5s" data-wow-delay="0.2s">
+                                <div class="">
+                                    <div class="our-story-content1 wow fadeInUp" data-wow-duration="0.5s"
+                                         data-wow-delay="0.5s">
+                                        <div class="our-story-content-inner">
+                                            <p>{{ $news->heading }}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="our-story wow fadeInUp"
-                             style="background: url({{ asset('uploads/blog_new2.jpeg') }}) no-repeat; background-position: center; background-size: cover;"
-                             data-wow-duration="0.5s" data-wow-delay="0.2s">
-                            <div class="">
-                                <div class="our-story-content2 wow fadeInUp" data-wow-duration="0.5s"
-                                     data-wow-delay="0.5s">
-                                    <div class="our-story-content-inner">
-                                        <p>Who Will Be Our Next Hero? Applications Are Open Now for Business Accelerator
-                                            Accelerator</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="our-story wow fadeInUp"
-                             style="background: url({{ asset('uploads/blog_new3.jpeg') }}) no-repeat; background-position: center; background-size: cover;"
-                             data-wow-duration="0.5s" data-wow-delay="0.2s">
-                            <div class="">
-                                <div class="our-story-content3 wow fadeInUp" data-wow-duration="0.5s"
-                                     data-wow-delay="0.5s">
-                                    <div class="our-story-content-inner">
-                                        <p>Business Accelerator Accelerator awards $1.1 million in funding to 11
-                                            startups at its startup showcase</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="our-story wow fadeInUp"
-                             style="background: url({{ asset('uploads/blog_new4.jpeg') }}) no-repeat; background-position: center; background-size: cover;"
-                             data-wow-duration="0.5s" data-wow-delay="0.2s">
-                            <div class="">
-                                <div class="our-story-content4 wow fadeInUp" data-wow-duration="0.5s"
-                                     data-wow-delay="0.5s">
-                                    <div class="our-story-content-inner">
-                                        <p>Meet the startups at this year’s Business Accelerator Accelerator Showcase
-                                            Accelerator Showcase</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @empty
+                    @endforelse
                 </div>
             </div>
         </section>
