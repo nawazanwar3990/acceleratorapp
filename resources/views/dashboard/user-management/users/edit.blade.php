6d852be6ddb44c1d21f2a2875faa45bb69aa01f9
nawazanwar3990 <86410602+nawazanwar3990@users.noreply.github.com>
2022-09-26 09:38:03 +0500

    refactor code

--- resources/views/dashboard/user-management/users/edit.blade.php
+++ resources/views/dashboard/user-management/users/edit.blade.php
@@ -5,7 +5,7 @@
             <div class="card shadow-none pt-0">
                 @include('dashboard.components.general.form-list-header')
                 <div class="card-body">
-                    {!! Form::model($model, ['url' =>route('dashboard.users.update', $model->id), 'method' => 'POST','files' => true,'id' =>'floorName', 'class' => 'solid-validation']) !!}
+                    {!! Form::model($user, ['url' =>route('dashboard.users.update', $user->id), 'method' => 'POST','files' => true,'id' =>'floorName', 'class' => 'solid-validation']) !!}
                     @method('PUT')
                     <x-updated-by-field></x-updated-by-field>
                     @include('dashboard.user-management.users.fields')
