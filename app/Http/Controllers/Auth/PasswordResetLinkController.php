634cdde2aae5802b22197cbb8517dc04f3a83d68
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-10-04 15:36:12 +0500

    Recovery Password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -27,7 +27,7 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
         return $status == Password::RESET_LINK_SENT
-            ? back()->with('status', __($status))
+            ? back()->with('success', __($status))
             : back()->withInput($request->only('email'))
                 ->withErrors(['email' => __($status)]);
     }
