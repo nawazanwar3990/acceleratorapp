3bbf1d950078291797645542652e4d13440d3207
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-10-04 15:34:48 +0500

    Recovery Password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -44,7 +44,7 @@ class NewPasswordController extends Controller
             }
         );
         return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
+            ? redirect()->route('login')->with('success', __($status))
             : back()->withInput($request->only('email'))
                 ->withErrors(['email' => __($status)]);
     }
