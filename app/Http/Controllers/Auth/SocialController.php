8e4028820f107a28e3517b7b659ca1f2ed2cce08
nawazanwar <66755713+nawazanwar@users.noreply.github.com>
2022-10-05 10:37:08 +0500

    Refactor Socialite Process

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -28,22 +28,23 @@ class SocialController extends Controller
     {
         try {
             $socialUser = Socialite::driver($provider)->stateless()->user();
+            echo "<pre>";
+            print_r($socialUser);
             $provider_id = $socialUser->id;
             $email = $socialUser->email;
             $is_register = $_COOKIE['is_register'];
-            if ($is_register == 'yes') {
+            /*if ($is_register == 'yes') {
                 // apply register
                 $alreadyUser = User::where('email', $email)->exists();
                 if ($alreadyUser) {
                     GeneralService::setCookie('is_register_error', 'yes');
                     return redirect()->route('website.index')->with('error', 'User With the Email Already Exists');
                 } else {
-                    echo "<pre>";
-                    print_r($socialUser);
+
                 }
             } else {
                 //apply login
-            }
+            }*/
         } catch (Exception $e) {
             return redirect()->route('social-login', array($provider));
         }
